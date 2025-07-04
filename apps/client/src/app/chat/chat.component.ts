796ef4721f3794f08e924ab65eef24d84498fa59
Luc Fauvel <luc.fauvel@hotmail.com>
2025-07-03 22:06:26 -0400

    Setup turbo repo

--- apps/client/src/app/chat/chat.component.ts
+++ apps/client/src/app/chat/chat.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { TrpcService } from '../services/trpc.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [],
+  providers: [TrpcService],
   templateUrl: './chat.component.html',
 })
 export class ChatComponent {
+  constructor(private trpc: TrpcService) {}
 
   sendMessage(message: Event): void {
   }
---
+++ apps/server/index.ts
@@ -0,0 +1,13 @@
+import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { router } from './trpc';
+ 
+const appRouter = router({
+  // ...
+});
+ 
+const server = createHTTPServer({
+  router: appRouter,
+});
+ 
+server.listen(3000);
+

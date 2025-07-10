09de74e91cd1778b77e67bf525f94da6bbfb66e8
Luc Fauvel <luc.fauvel@hotmail.com>
2025-07-10 19:15:39 -0400

    Adding prompting

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -2,13 +2,32 @@ import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import { publicProcedure, router } from './trpc';
 import z from 'zod';
 import cors from 'cors';
+import { GoogleGenAI } from '@google/genai';
+import dotenv from 'dotenv';
+
+dotenv.config();
  
+const genAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY as string });
+
 const appRouter = router({
   askChat: publicProcedure
     .input(z.string())
     .query(async ({ input }) => {
-      // Simulate a chat response
-      return `You asked: ${input}`;
+      const result = await genAI.models.generateContent(
+      { 
+        model: "gemini-1.5-flash",
+        contents: input,
+        config: { 
+          systemInstruction: [
+            `You are an engineer and CAD expert.`,
+            `You are specifically trained to write OpenSCAD code.`,
+            `When you receive a prompt, you will respond with OpenSCAD code that fulfills the request.`,
+            `You will only respond with OpenSCAD code, and nothing else.`,
+            `If you do not know how to write the code, you will respond with an empty string.`
+          ] 
+        } 
+      });
+      return result.text;
     })
 });
 

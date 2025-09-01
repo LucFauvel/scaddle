8320219394f9f893283a4cb556713404370c5ff2
Luc Fauvel <luc.fauvel@hotmail.com>
2025-08-31 20:53:04 -0400

    Added protected route

--- apps/server/trpc.ts
+++ apps/server/trpc.ts
@@ -1,15 +1,42 @@
-import { initTRPC } from '@trpc/server';
- 
-/**
- * Initialization of tRPC backend
- * Should be done only once per backend!
- */
-const t = initTRPC.create();
- 
-/**
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
-export const router = t.router;
-export const publicProcedure = t.procedure;
+import { initTRPC, TRPCError } from '@trpc/server';
+import { auth } from './auth'
+import { ZodError } from 'better-auth';
+import { z } from 'zod';
 
+export const createTRPCContext = async (opts: { headers: Headers }) => {
+  const authSession = await auth.api.getSession({
+    headers: opts.headers
+  })
+
+  const source = opts.headers.get('x-trpc-source') ?? 'unknown'
+  console.log('>>> tRPC Request from', source, 'by', authSession?.user.email)
+
+  return {
+    user: authSession?.user
+  }
+}
+type Context = Awaited<ReturnType<typeof createTRPCContext>>
+const t = initTRPC.context<Context>().create({
+  errorFormatter: ({ shape, error }) => ({
+    ...shape,
+    data: {
+      ...shape.data,
+      zodError: error.cause instanceof ZodError ? z.treeifyError(error.cause) : null
+    }
+  })
+})
+
+export const createCallerFactory = t.createCallerFactory
+export const createTRPCRouter = t.router
+export const publicProcedure = t.procedure
+
+export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+  if (!ctx.user?.id) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+  return next({
+    ctx: {
+      user: ctx.user
+    }
+  })
+})
